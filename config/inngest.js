8e501c2653dab4e3ae4567fc15cf646d18b5aa07
uttam06 <dahaluttam06@gmail.com>
2025-03-14 15:45:38 +0545

    missing event handeling

--- config/inngest.js
+++ config/inngest.js
@@ -14,7 +14,7 @@ export const syncUserCreation = inngest.createFunction(
   {
     event: 'clerk/user.created'
   },
-  async (event) => {
+  async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data
     const userData = {
       _id: id,
