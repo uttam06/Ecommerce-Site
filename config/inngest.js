334ccb81c2cde4b5b2862dba395b82bd331a751c
uttam06 <dahaluttam06@gmail.com>
2025-03-14 15:03:26 +0545

    removing a function from inngest client

--- config/inngest.js
+++ config/inngest.js
@@ -66,32 +66,4 @@ export const syncUserDeletion = inngest.createFunction(
 )
 
 
-//inngest function to create user's order in database
 
-export const createUserOrder = inngest.createFunction(
-  {
-    id: 'create-user-order',
-    batchEvents: {
-      maxSize: 5,
-      timeout: '10s'
-    }
-  },
-  { event: 'order/created' },
-  async ({ events }) => {
-    const orders = events.map((event) => {
-      return {
-        userId: event.data.userId,
-        items: event.data.items,
-        amount: event.data.amount,
-        address: event.data.address,
-        date: event.data.date
-      }
-    })
-
-    await connectDB()
-    await Order.insertMany(orders)
-
-    return { success: true, processed: orders.length };
-
-  }
-)
