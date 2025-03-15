5b5b4508b80010aa188b4e1acb1e7b267e748fea
uttam06 <dahaluttam06@gmail.com>
2025-03-15 20:07:31 +0545

    fixing order section

--- app/seller/orders/page.jsx
+++ app/seller/orders/page.jsx
@@ -1,88 +1,173 @@
-'use client';
-import React, { useEffect, useState } from "react";
+'use client'
+import React, { useState } from "react";
+import { assets } from "@/assets/assets";
 import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
-import Footer from "@/components/seller/Footer";
-import Loading from "@/components/Loading";
 import axios from "axios";
+import toast from "react-hot-toast";
 
-const Orders = () => {
+const AddProduct = () => {
 
-  const { currency, getToken, user } = useAppContext();
+  const { getToken } = useAppContext();
 
-  const [orders, setOrders] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [files, setFiles] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('Earphone');
+  const [price, setPrice] = useState('');
+  const [offerPrice, setOfferPrice] = useState('');
 
-  const fetchSellerOrders = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append('name', name)
+    formData.append('description', description)
+    formData.append('category', category)
+    formData.append('price', price)
+    formData.append('offerPrice', offerPrice)
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append('images', files[i])
+    }
+
     try {
       const token = await getToken()
-
-      const { data } = await axios.get('/api/order/seller-orders', { headers: { Authorization: `Bearer ${token}` } })
+      const { data } = await axios.post('/api/product/add', formData, { headers: { Authorization: `Bearer ${token}` } })
 
       if (data.success) {
-        setOrders(data.orders)
-        setLoading(false);
+        toast.success(data.message)
+        setFiles([]);
+        setName('');
+        setDescription('');
+        setCategory('Earphone');
+        setPrice('');
+        setOfferPrice('');
       } else {
-        toast.error(data.message)
+        toast.error(data.message);
       }
+
     } catch (error) {
-
-      toast.error(error.message)
+      toast.error(error.message);
     }
-  }
 
-  useEffect(() => {
-    if (user) {
-      fetchSellerOrders();
-    }
-  }, [user]);
+
+  };
 
   return (
-    <div className="flex-1 h-screen overflow-scroll flex flex-col justify-between text-sm">
-      {loading ? <Loading /> : <div className="md:p-10 p-4 space-y-5">
-        <h2 className="text-lg font-medium">Orders</h2>
-        <div className="max-w-4xl rounded-md">
-          {orders.map((order, index) => (
-            <div key={index} className="flex flex-col md:flex-row gap-5 justify-between p-5 border-t border-gray-300">
-              <div className="flex-1 flex gap-5 max-w-80">
+    <div className="flex-1 min-h-screen flex flex-col justify-between">
+      <form onSubmit={handleSubmit} className="md:p-10 p-4 space-y-5 max-w-lg">
+        <div>
+          <p className="text-base font-medium">Product Image</p>
+          <div className="flex flex-wrap items-center gap-3 mt-2">
+
+            {[...Array(4)].map((_, index) => (
+              <label key={index} htmlFor={`image${index}`}>
+                <input onChange={(e) => {
+                  const updatedFiles = [...files];
+                  updatedFiles[index] = e.target.files[0];
+                  setFiles(updatedFiles);
+                }} type="file" id={`image${index}`} hidden />
                 <Image
-                  className="max-w-16 max-h-16 object-cover"
-                  src={assets.box_icon}
-                  alt="box_icon"
+                  key={index}
+                  className="max-w-24 cursor-pointer"
+                  src={files[index] ? URL.createObjectURL(files[index]) : assets.upload_area}
+                  alt=""
+                  width={100}
+                  height={100}
                 />
-                <p className="flex flex-col gap-3">
-                  <span className="font-medium">
-                    {order.items.map((item) => item.product.name + ` x ${item.quantity}`).join(", ")}
-                  </span>
-                  <span>Items : {order.items.length}</span>
-                </p>
-              </div>
-              <div>
-                <p>
-                  <span className="font-medium">{order.address.fullName}</span>
-                  <br />
-                  <span >{order.address.area}</span>
-                  <br />
-                  <span>{`${order.address.city}, ${order.address.state}`}</span>
-                  <br />
-                  <span>{order.address.phoneNumber}</span>
-                </p>
-              </div>
-              <p className="font-medium my-auto">{currency}{order.amount}</p>
-              <div>
-                <p className="flex flex-col">
-                  <span>Method : COD</span>
-                  <span>Date : {new Date(order.date).toLocaleDateString()}</span>
-                  <span>Payment : Pending</span>
-                </p>
-              </div>
-            </div>
-          ))}
+              </label>
+            ))}
+
+          </div>
         </div>
-      </div>}
-      <Footer />
+        <div className="flex flex-col gap-1 max-w-md">
+          <label className="text-base font-medium" htmlFor="product-name">
+            Product Name
+          </label>
+          <input
+            id="product-name"
+            type="text"
+            placeholder="Type here"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-1 max-w-md">
+          <label
+            className="text-base font-medium"
+            htmlFor="product-description"
+          >
+            Product Description
+          </label>
+          <textarea
+            id="product-description"
+            rows={4}
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none"
+            placeholder="Type here"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            required
+          ></textarea>
+        </div>
+        <div className="flex items-center gap-5 flex-wrap">
+          <div className="flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="category">
+              Category
+            </label>
+            <select
+              id="category"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              onChange={(e) => setCategory(e.target.value)}
+              defaultValue={category}
+            >
+              <option value="Earphone">Earphone</option>
+              <option value="Headphone">Headphone</option>
+              <option value="Watch">Watch</option>
+              <option value="Smartphone">Smartphone</option>
+              <option value="Laptop">Laptop</option>
+              <option value="Camera">Camera</option>
+              <option value="Accessories">Accessories</option>
+            </select>
+          </div>
+          <div className="flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="product-price">
+              Product Price
+            </label>
+            <input
+              id="product-price"
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="offer-price">
+              Offer Price
+            </label>
+            <input
+              id="offer-price"
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              onChange={(e) => setOfferPrice(e.target.value)}
+              value={offerPrice}
+              required
+            />
+          </div>
+        </div>
+        <button type="submit" className="px-8 py-2.5 bg-orange-600 text-white font-medium rounded">
+          ADD
+        </button>
+      </form>
+      {/* <Footer /> */}
     </div>
   );
 };
 
-export default Orders;
+export default AddProduct;
